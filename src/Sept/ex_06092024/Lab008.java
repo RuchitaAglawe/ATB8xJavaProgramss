bfa89ee0d8c1f16e5f1b85b367e73e61394034dd
Ruchita Aglawe <aglaweruchita@gmail.com>
2024-09-15 10:27:08 +0530

    Add the files

---
+++ src/Sept/ex_06092024/Lab008.java
@@ -0,0 +1,12 @@
+//
+package Sept.ex_06092024;
+/*
+*Write here anything not run.
+ */
+public class Lab008 {
+    //Anything write here is not run
+    public static void main(String[] args) {
+        //here also write anything
+        System.out.println("Hello World");
+    }
+}
