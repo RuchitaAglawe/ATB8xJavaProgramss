958ef84832f1dd5c122b91ecd6ca6a9a2778e98a
Ruchita Aglawe <aglaweruchita@gmail.com>
2024-09-17 21:31:59 +0530

    All the files - 11 sept

---
+++ src/Sept/ex_11092024/Lab014.java
@@ -0,0 +1,14 @@
+package Sept.ex_11092024;
+
+public class Lab014 {
+    public static void main(String[] args) {
+        //Size - how much they can store.
+        byte b = 127;
+        short s = 128;
+
+        byte age = 127;
+        short s1 = 128;
+
+        System.out.println(b);
+    }
+}
