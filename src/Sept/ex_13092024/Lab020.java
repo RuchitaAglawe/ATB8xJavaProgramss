ebab195e5da3b84918c7a6cc8d45163b140be845
Ruchita Aglawe <aglaweruchita@gmail.com>
2024-09-18 08:40:53 +0530

    All the files add

---
+++ src/Sept/ex_13092024/Lab020.java
@@ -0,0 +1,10 @@
+package Sept.ex_13092024;
+
+public class Lab020 {
+    public static void main(String[] args) {
+        // Problem statement - print the table of 9
+        System.out.println("9*1=9");
+        System.out.println("9*2=18");
+        System.out.println("9*10=90");
+    }
+}
---
+++ src/Sept/ex_13092024/Lab026.java
@@ -0,0 +1,13 @@
+package Sept.ex_13092024;
+
+public class Lab026 {
+    public static void main(String[] args) {
+        // Constant
+        // constant vs variables
+        // whose values cannot be changed once they are assigned.
+        // final - final dataType CONSTANT_NAME = value;
+        final float PIE = 3.1456f;
+        //PIE = 12.34f;
+        System.out.println(PIE);
+    }
+}
